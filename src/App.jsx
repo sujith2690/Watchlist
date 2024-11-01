f8d5a28523b05e6fbf05659a9fb8aeb304c69053
Sujith P <sujithp2690@gmail.com>
2024-11-02 00:59:36 +0530

    routes are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Loading from './components/pages/Loading'
 
 const App = () => {
+  const Home = lazy(() => import('./components/pages/HomePage'))
+  const Login = lazy(() => import('./components/pages/LoginPage'))
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="h-screen text-white  bg-[#050E17] ">
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/auth' element={<Login />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </>
   )
 }
 
